44255fda4a00d03df7e7ffe63f6ff739914a9729
leofontolan <leofontolan@gmail.com>
2022-08-09 11:14:54 -0300

    Module PATH

--- aula02/app.js
+++ aula02/app.js
@@ -1,4 +1,11 @@
+const path = require('path');
+console.log(path.resolve(__dirname, '..', '..'));
+
+
 const Dog = require('./b/c/d/mod.js');
 
 const dog = new Dog('Fido');
 console.log(dog.speak());
+
+console.log(__filename);
+console.log(__dirname);
