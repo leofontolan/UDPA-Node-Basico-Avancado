7e755cf885c19d5ac87118f845830b25638c4b0f
leofontolan <leofontolan@gmail.com>
2022-08-10 11:24:26 -0300

    Duplicando projeto

---
+++ aula07/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+        <form action="/" method="post">
+        Nome: <input type="text" name="name" placeholder="name">
+        <button type="submit">Enviar</button>
+    `);
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send('Recebi o seu nome: ');
+});
+
+
+
+app.get("/contact", (req, res) => {
+  res.send('Contact me at: <a href="mailto:leo@fontolan.com.br">');
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
