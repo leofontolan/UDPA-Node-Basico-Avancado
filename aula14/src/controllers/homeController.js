da2127a4c542f65aa004af0e27e55b05eb5de8da
leofontolan <leofontolan@gmail.com>
2022-08-16 11:45:58 -0300

    Criando tabela no MongoDB

--- aula14/src/controllers/homeController.js
+++ aula14/src/controllers/homeController.js
@@ -1,11 +1,20 @@
+const HomeModel = require("../models/HomeModel");
+
+HomeModel.create({
+  titulo: "Um título de teste",
+  descricao: "Uma descrição de teste",
+})
+  .then((dados) => console.log(dados))
+  .catch((erro) => console.log(erro));
+
 exports.paginaInicial = (req, res) => {
-    res.render('index');
-    return;
+  res.render("index");
+  return;
 };
 
 exports.trataPost = (req, res) => {
-    res.send(`
+  res.send(`
         <h1>Ola ${req.body.name}</h1>
     `);
-    return;
+  return;
 };
---
+++ aula14/src/models/HomeModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const HomeSchema = new mongoose.Schema({
+
+    titulo: { type: String, required: true },
+    descricao: { type: String, required: true },
+
+});
+
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+module.exports = HomeModel;
